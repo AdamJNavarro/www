7cb858847c1edc6800b8c253a12bd33399d21cdb
Adam Navarro <adamnavarrowisc@gmail.com>
2021-04-27 14:07:08 -0500

    Initial push

---
+++ src/config/routes.ts
@@ -0,0 +1,12 @@
+const routes = {
+    home: {
+        label: "Home",
+        path: "/"
+    },
+    about: {
+        label: "About",
+        path: "/about"
+    }
+}
+
+export default routes
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,11 @@
 import Head from 'next/head'
+import Link from 'next/link'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
+
+
     <div className={styles.container}>
       <Head>
         <title>Create Next App</title>
@@ -11,7 +14,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Go to <Link href="/about"><a>About page.</a></Link>
         </h1>
 
         <p className={styles.description}>
