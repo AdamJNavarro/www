f3bd75af96968e671cdaea444c604bfcb2dd758b
Adam Navarro <adamnavarrowisc@gmail.com>
2021-05-04 16:53:54 -0500

    + Palette & refactor Active Projects List code

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,5 +1,26 @@
 import { CenteredColumn, Page } from "../components/layout";
 
+const activeProjects = [
+  {
+    name: "Expo",
+    description:
+      "A framework and a platform for universal React applications. It is a set of tools and services built around React Native and native platforms that help you develop, build, deploy, and quickly iterate on iOS, Android, and web apps from the same JavaScript/TypeScript codebase.",
+    url: "https://expo.io/home",
+  },
+  {
+    name: "Momento",
+    description:
+      "A mobile application designed to help people create lasting video memories to be shared with loved ones.",
+    url: "https://getmomento.netlify.app/",
+  },
+  {
+    name: "Palette",
+    description:
+      "Quickly test color palettes and check contrast accessibility scores when designing themes for applications. Built with Expo. A work in progress.",
+    url: "https://expo.io/@adamjnav/projects/palette",
+  },
+];
+
 export default function ProjectsPage() {
   return (
     <Page>
@@ -18,7 +39,7 @@ export default function ProjectsPage() {
               <h4 className="font-sans text-xl font-bold md:text-2xl text-secondary">
                 Active Projects
               </h4>
-              <ActiveProjectList />
+              <ActiveProjectsList />
             </div>
             <div className="space-y-8">
               <h4 className="font-sans text-xl font-bold md:text-2xl text-secondary">
@@ -33,43 +54,25 @@ export default function ProjectsPage() {
   );
 }
 
-const ActiveProjectList = () => {
+const ActiveProjectsList = () => {
   return (
     <div className="prose space-y-6">
-      <div className="space-y-1">
-        <a
-          href="https://expo.io/home"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="font-medium highlight-link-hover"
-        >
-          Expo
-        </a>
-
-        <p className="text-tertiary">
-          A framework and a platform for universal React applications. It is a
-          set of tools and services built around React Native and native
-          platforms that help you develop, build, deploy, and quickly iterate on
-          iOS, Android, and web apps from the same JavaScript/TypeScript
-          codebase.
-        </p>
-      </div>
-
-      <div className="space-y-1">
-        <a
-          href="https://getmomento.netlify.app/"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="font-medium highlight-link-hover"
-        >
-          Momento
-        </a>
-
-        <p className="text-tertiary">
-          A mobile application designed to help people create lasting video
-          memories to be shared with loved ones.
-        </p>
-      </div>
+      {activeProjects.map((project) => {
+        const { name, url, description } = project;
+        return (
+          <div className="space-y-1">
+            <a
+              href={url}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="font-medium highlight-link-hover"
+            >
+              {name}
+            </a>
+            <p className="text-tertiary">{description}</p>
+          </div>
+        );
+      })}
     </div>
   );
 };
