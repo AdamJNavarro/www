ae7653e2b245f320b250793f1a82436be61ac2c5
Adam Navarro <adamjnavarro@icloud.com>
2021-05-28 18:55:54 -0500

    Add fathom client

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,19 @@
 import "~/styles/tailwind.css"
 import "~/styles/prose.css"
 
+import * as Fathom from "fathom-client"
+
 import MetaWrapper from "~/components/layout/MetaWrapper"
+import { useEffect } from "react"
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    Fathom.load("TOSBYDZC", {
+      includedDomains: ["adamjnavarro.com"],
+      excludedDomains: ["localhost", "now.sh"],
+    })
+  }, [])
+
   return (
     <MetaWrapper>
       <Component {...pageProps} />
