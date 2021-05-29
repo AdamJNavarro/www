0d747879a0f129c1e6ab2aef8b9b7ac50d704853
Adam Navarro <adamjnavarro@icloud.com>
2021-05-28 19:08:00 -0500

    Impl page view tracking

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,13 +5,26 @@ import * as Fathom from "fathom-client"
 
 import MetaWrapper from "~/components/layout/MetaWrapper"
 import { useEffect } from "react"
+import { useRouter } from "next/router"
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter()
   useEffect(() => {
     Fathom.load("TOSBYDZC", {
       includedDomains: ["adamjnavarro.com"],
       excludedDomains: ["localhost", "now.sh"],
     })
+
+    function onRouteChangeComplete() {
+      Fathom.trackPageview()
+    }
+    // Record a pageview when route changes
+    router.events.on("routeChangeComplete", onRouteChangeComplete)
+
+    // Unassign event listener
+    return () => {
+      router.events.off("routeChangeComplete", onRouteChangeComplete)
+    }
   }, [])
 
   return (
