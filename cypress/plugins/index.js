5abe969bf8e29fa5844b2ac23434f57c410b3a1a
Adam Navarro <adamjnavarro@icloud.com>
2021-06-22 11:22:06 -0500

    Impl base cypress config

---
+++ cypress/plugins/index.js
@@ -0,0 +1,8 @@
+/// <reference types="cypress" />
+
+const injectDevServer = require('@cypress/react/plugins/next')
+
+module.exports = (on, config) => {
+  injectDevServer(on, config)
+  return config
+}
