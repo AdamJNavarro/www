3867705303765ef1132995cfca548e5d26870760
Adam Navarro <adamjnav@gmail.com>
2021-10-21 19:26:47 -0500

    chore: remove next-plausible

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,14 +2,11 @@ import '~/styles/tailwind.css';
 import '~/styles/prose.css';
 
 import MetaWrapper from '~/components/layout/MetaWrapper';
-//import PlausibleProvider from "next-plausible";
 
 function MyApp({ Component, pageProps }) {
   return (
     <MetaWrapper>
-      {/* <PlausibleProvider domain="adamjnavarro.com"> */}
       <Component {...pageProps} />
-      {/* </PlausibleProvider> */}
     </MetaWrapper>
   );
 }
