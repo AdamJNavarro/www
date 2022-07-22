b607e37586ca337c65eae37897f62cbfb07c3d17
Adam Navarro <adamjnav@gmail.com>
2022-07-22 13:45:08 -0500

    chore: add remaing parts of speech to word bank

--- src/components/Words/Words.data.ts
+++ src/components/Words/Words.data.ts
@@ -1,20 +1,5 @@
 import { WordProps } from './Words.types';
 
-const API_KEY = process.env.NEXT_PUBLIC_MERRIAM_WEBSTER_KEY;
-
-export async function getDictionaryData(spelling: string) {
-  const response = await fetch(
-    `https://www.dictionaryapi.com/api/v3/references/collegiate/json/${spelling}?key=${API_KEY}`,
-    {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json;charset=utf-8',
-      },
-    }
-  );
-  const data = await response.json();
-}
-
 const wordBank: WordProps[] = [
   {
     spelling: 'ameliorate',
@@ -25,13 +10,13 @@ const wordBank: WordProps[] = [
   {
     spelling: 'anathema',
     definition: 'someone or something that is detested or shunned',
-    //partOfSpeech: '',
+    partOfSpeech: 'noun',
     dateLearned: 1578715080,
   },
   {
-    spelling: 'appelation',
+    spelling: 'appellation',
     definition: 'a name or title',
-    //partOfSpeech: '',
+    partOfSpeech: 'noun',
     dateLearned: 1610337360,
   },
   {
@@ -43,45 +28,45 @@ const wordBank: WordProps[] = [
   {
     spelling: 'assiduously',
     definition: 'with great care and perseverance',
-    //partOfSpeech: '',
+    partOfSpeech: 'adverb',
     dateLearned: 1578714300,
   },
   {
     spelling: 'autarky',
     definition: 'economic independence or self-sufficiency',
-    //partOfSpeech: '',
+    partOfSpeech: 'noun',
     dateLearned: 1547178000,
   },
   {
     spelling: 'bien-pensants',
     definition: 'a right thinking or orthodox person',
-    //partOfSpeech: '',
+    partOfSpeech: 'noun',
     dateLearned: 1515643140,
   },
   {
     spelling: 'bivouac',
     definition:
       'a temporary camp without tents or cover, used especially by soldiers or mountaineers',
-    //partOfSpeech: '',
+    partOfSpeech: 'noun',
     dateLearned: 1547177940,
   },
   {
     spelling: 'cogitating',
     definition: 'pondering or meditating on',
-    //partOfSpeech: '',
+    partOfSpeech: 'verb',
     dateLearned: 1515642480,
   },
   {
     spelling: 'cognomen',
     definition:
       'an extra personal name given to an ancient Roman citizen; functioning like a nickname',
-    //partOfSpeech: '',
+    partOfSpeech: 'noun',
     dateLearned: 1610337300,
   },
   {
     spelling: 'condign',
     definition: 'appropriate to the punishment of the crime, fitting, deserved',
-    //partOfSpeech: '',
+    partOfSpeech: 'adjective',
     dateLearned: 1578714600,
   },
   {
@@ -93,139 +78,139 @@ const wordBank: WordProps[] = [
   {
     spelling: 'contrivance',
     definition: 'an artificial arrangement or development',
-    //partOfSpeech: '',
+    partOfSpeech: 'noun',
     dateLearned: 1547178480,
   },
   {
     spelling: 'cyclamen',
     definition: 'plants of the primrose family having showy nodding flowers',
-    //partOfSpeech: '',
+    partOfSpeech: 'noun',
     dateLearned: 1610337120,
   },
   {
     spelling: 'demiurge',
     definition: 'a being responsible for the creation of the universe',
-    //partOfSpeech: '',
+    partOfSpeech: 'noun',
     dateLearned: 1578715020,
   },
   {
     spelling: 'desiccate',
     definition: 'to dry thoroughly',
-    //partOfSpeech: '',
+    partOfSpeech: 'verb',
     dateLearned: 1578714420,
   },
   {
     spelling: 'ermine',
     definition: 'any of several weasels whose coats become white in winter',
-    //partOfSpeech: '',
+    partOfSpeech: 'noun',
     dateLearned: 1547179020,
   },
   {
     spelling: 'exigency',
     definition: 'an urgent need or demand',
-    //partOfSpeech: '',
+    partOfSpeech: 'noun',
     dateLearned: 1547179140,
   },
   {
     spelling: 'festoon',
     definition: 'to adorn a place with ribbons, garlands, or other decorations',
-    //partOfSpeech: '',
+    partOfSpeech: 'verb',
     dateLearned: 1547178360,
   },
   {
     spelling: 'furtive',
     definition:
       'attempting to avoid notice or attention, typically because of guilt or a belief that discovery would lead to trouble; secretive',
-    //partOfSpeech: '',
+    partOfSpeech: 'adjective',
     dateLearned: 1547178060,
   },
   {
     spelling: 'fusillade',
     definition:
       'a series of shots fired or missiles thrown all at the same time or in quick succession',
-    //partOfSpeech: '',
+    partOfSpeech: 'noun',
     dateLearned: 1578714780,
   },
   {
     spelling: 'gauche',
     definition: 'lacking social experience or grace',
-    //partOfSpeech: '',
+    partOfSpeech: 'adjective',
     dateLearned: 1515642600,
   },
   {
     spelling: 'hegemonic',
     definition: 'having complete dominion or power',
-    //partOfSpeech: '',
+    partOfSpeech: 'noun',
     dateLearned: 1610337060,
   },
   {
     spelling: 'iconoclast',
     definition: 'a person who attacks cherished beliefs or institutions',
-    //partOfSpeech: '',
+    partOfSpeech: 'noun',
     dateLearned: 1578714840,
   },
   {
-    spelling: 'impingement',
+    spelling: 'impinge',
     definition:
       'to have an impact on or make an impression; to encroach or infringe upon',
-    //partOfSpeech: '',
+    partOfSpeech: 'verb',
     dateLearned: 1578714180,
   },
   {
     spelling: 'improvident',
     definition: 'not foreseeing and providing for the future',
-    //partOfSpeech: '',
+    partOfSpeech: 'adjective',
     dateLearned: 1547178540,
   },
   {
     spelling: 'inimitable',
     definition: 'so good or unusual as to be impossible to copy; unique',
-    //partOfSpeech: '',
+    partOfSpeech: 'adjective',
     dateLearned: 1578714720,
   },
   {
     spelling: 'iniquity',
     definition: 'immoral or grossly unfair behavior',
-    //partOfSpeech: '',
+    partOfSpeech: 'noun',
     dateLearned: 1610337420,
   },
   {
     spelling: 'intransigence',
     definition: "refusal to change one's views or to agree about something",
-    //partOfSpeech: '',
+    partOfSpeech: 'noun',
     dateLearned: 1547178180,
   },
   {
     spelling: 'jingoism',
     definition:
       'extreme patriotism, most often demonstrated via aggressive foreign policy',
-    //partOfSpeech: '',
+    partOfSpeech: 'noun',
     dateLearned: 1630472400,
   },
   {
     spelling: 'luddite',
     definition:
       'someone who is opposed or resistant to new technologies or technological change',
-    //partOfSpeech: '',
+    partOfSpeech: 'noun',
     dateLearned: 1578714960,
   },
   {
     spelling: 'melange',
     definition: 'a mixture or medley',
-    //partOfSpeech: '',
+    partOfSpeech: 'noun',
     dateLearned: 1578714480,
   },
   {
     spelling: 'nascent',
     definition:
       'coming into existence and beginning to display signs of future potential',
-    //partOfSpeech: '',
+    partOfSpeech: 'adjective',
     dateLearned: 1578715140,
   },
   {
     spelling: 'obsequious',
     definition: 'marked by or exhibiting a fawning attentiveness',
-    //partOfSpeech: '',
+    partOfSpeech: 'adjective',
     dateLearned: 1547178900,
   },
   {
@@ -237,162 +222,162 @@ const wordBank: WordProps[] = [
   {
     spelling: 'pedagogy',
     definition: 'the art, science, or profession of teaching',
-    //partOfSpeech: '',
+    partOfSpeech: 'noun',
     dateLearned: 1547178720,
   },
   {
     spelling: 'pernicious',
     definition: 'having a harmful effect in a gradual or subtle manner',
-    //partOfSpeech: '',
+    partOfSpeech: 'adjective',
     dateLearned: 1578714660,
   },
   {
     spelling: 'perspicacious',
     definition: 'having a ready insight into and understanding of things',
-    //partOfSpeech: '',
+    partOfSpeech: 'adjective',
     dateLearned: 1610337540,
   },
   {
     spelling: 'pollyannish',
     definition:
       'irrepressibly or excessively optimistic; capable of seeing good in anyone',
-    //partOfSpeech: '',
+    partOfSpeech: 'adjective',
     dateLearned: 1515642780,
   },
   {
     spelling: 'presage',
     definition: 'something that foreshadows or portends a future event',
-    //partOfSpeech: '',
+    partOfSpeech: 'noun',
     dateLearned: 1515642900,
   },
   {
     spelling: 'prevaricate',
     definition: 'to deviate from the truth',
-    //partOfSpeech: '',
+    partOfSpeech: 'verb',
     dateLearned: 1515642360,
   },
   {
     spelling: 'prig',
     definition: 'one who offends or irritates',
-    //partOfSpeech: '',
+    partOfSpeech: 'noun',
     dateLearned: 1610337180,
   },
   {
     spelling: 'prolegomenon',
     definition:
       'a formal essay or critical discussion serving to introduce and interpret an extended work',
-    //partOfSpeech: '',
+    partOfSpeech: 'noun',
     dateLearned: 1610337240,
   },
   {
     spelling: 'putative',
     definition: 'generally considered or reputed to be',
-    //partOfSpeech: '',
+    partOfSpeech: 'adjective',
     dateLearned: 1578714360,
   },
   {
     spelling: 'querulous',
     definition: 'habitually complaining',
-    //partOfSpeech: '',
+    partOfSpeech: 'adjective',
     dateLearned: 1547178840,
   },
   {
     spelling: 'reconnoiter',
     definition: 'to make a military observation of (a region)',
-    //partOfSpeech: '',
+    partOfSpeech: 'verb',
     dateLearned: 1547178300,
   },
   {
     spelling: 'recrimination',
     definition: 'a retaliatory accusation',
-    //partOfSpeech: '',
+    partOfSpeech: 'noun',
     dateLearned: 1515642660,
   },
   {
     spelling: 'remonstrance',
     definition: 'an earnest presentation of reasons for opposition or grievance',
-    //partOfSpeech: '',
+    partOfSpeech: 'noun',
     dateLearned: 1515643080,
   },
   {
     spelling: 'rivulet',
     definition: 'a small stream',
-    //partOfSpeech: '',
+    partOfSpeech: 'noun',
     dateLearned: 1515642720,
   },
   {
     spelling: 'scansion',
-    definition: ' the action of scanning a line of verse to determine its rhythm',
-    //partOfSpeech: '',
+    definition: 'the action of scanning a line of verse to determine its rhythm',
+    partOfSpeech: 'noun',
     dateLearned: 1547178240,
   },
   {
     spelling: 'seiche',
     definition:
       'an oscillation of the surface of a landlocked body of water that varies in period from a few minutes to several hours',
-    //partOfSpeech: '',
+    partOfSpeech: 'noun',
     dateLearned: 1547178600,
   },
   {
     spelling: 'semaphore',
     definition:
       'a variable or abstract data type used to control access to a common resource',
-    //partOfSpeech: '',
+    partOfSpeech: 'noun',
     dateLearned: 1515642960,
   },
   {
     spelling: 'shibboleth',
     definition:
       'a custom, principle, or belief distinguishing a particular class or group of people, especially a long-standing one regarded as outmoded or no longer important',
-    //partOfSpeech: '',
+    partOfSpeech: 'noun',
     dateLearned: 1578714900,
   },
   {
     spelling: 'sidled',
     definition: 'to move sideways',
-    //partOfSpeech: '',
+    partOfSpeech: 'verb',
     dateLearned: 1515642540,
   },
   {
     spelling: 'slovenly',
     definition: 'careless; excessively casual',
-    //partOfSpeech: '',
+    partOfSpeech: 'adjective',
     dateLearned: 1547178420,
   },
   {
     spelling: 'somnolence',
     definition: 'the quality or state of being drowsy',
-    //partOfSpeech: '',
+    partOfSpeech: 'noun',
     dateLearned: 1547178960,
   },
   {
     spelling: 'soporific',
     definition: 'causing or tending to cause sleep',
-    //partOfSpeech: '',
+    partOfSpeech: 'adjective',
     dateLearned: 1547178780,
   },
   {
     spelling: 'suffuse',
     definition: 'gradually spread through or over',
-    //partOfSpeech: '',
+    partOfSpeech: 'verb',
     dateLearned: 1610337480,
   },
   {
-    spelling: 'synodical',
+    spelling: 'synodic',
     definition: 'relating to conjunction',
-    //partOfSpeech: '',
+    partOfSpeech: 'adjective',
     dateLearned: 1547179080,
   },
   {
     spelling: 'timorously',
     definition: 'of a timid disposition',
-    //partOfSpeech: '',
+    partOfSpeech: 'adjective',
     dateLearned: 1547178660,
   },
   {
     spelling: 'umbrage',
     definition: 'offense or annoyance',
-    //partOfSpeech: '',
+    partOfSpeech: 'noun',
     dateLearned: 1578714540,
   },
   {
@@ -404,7 +389,7 @@ const wordBank: WordProps[] = [
   {
     spelling: 'vituperation',
     definition: 'noun that refers to critical or abusive language',
-    //partOfSpeech: '',
+    partOfSpeech: 'noun',
     dateLearned: 1515642840,
   },
   {
