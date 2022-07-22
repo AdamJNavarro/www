e0564c78ff23018e311a9845f85110267d2d2db3
Adam Navarro <adamjnav@gmail.com>
2022-07-22 16:50:53 -0500

    chore: create custom fetch fx with err handling

---
+++ src/utils/Data.ts
@@ -0,0 +1,34 @@
+interface GoFetchArgs {
+  url: string;
+  config: RequestInit;
+}
+
+interface GoFetchPayload {
+  data?: any;
+  error?: string;
+}
+
+export async function goFetch({
+  url,
+  config,
+}: GoFetchArgs): Promise<GoFetchPayload> {
+  try {
+    const resp = await fetch(url, config);
+    const isJson = resp.headers.get('content-type')?.includes('application/json');
+    const data = isJson ? await resp.json() : null;
+
+    if (!resp.ok) {
+      const statusCode = resp.status;
+      const error = (data && data.message) || `Error Code: ${statusCode}`;
+      throw error;
+    }
+
+    return {
+      data,
+    };
+  } catch (e) {
+    return {
+      error: e,
+    };
+  }
+}
