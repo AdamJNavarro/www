c439df76c83b8bccfac81de90c75468c66f4f032
Adam Navarro <adamjnav@gmail.com>
2022-07-27 11:53:29 -0500

    chore: update bio with real content

--- src/components/About/Biography.tsx
+++ src/components/About/Biography.tsx
@@ -4,21 +4,12 @@ export default function Biography() {
   return (
     <Typography>
       <p>
-        You will never miss important product updates, latest news and community QA
-        sessions. Our newsletter is once a week, every Sunday. You will never miss
-        important product updates, latest news and community QA sessions. Our
-        newsletter is once a week, every Sunday. You will never miss important
-        product updates, latest news and community QA sessions. Our newsletter is
-        once a week, every Sunday. You will never miss important product updates,
-        latest news and community QA sessions. Our newsletter is once a week, every
-        Sunday. You will never miss important product updates, latest news and
-        community QA sessions. Our newsletter is once a week, every Sunday.
-      </p>
-      <p>
-        You will never miss important product updates, latest news and community QA
-        sessions. Our newsletter is once a week, every Sunday. You will never miss
-        important product updates, latest news and community QA sessions. Our
-        newsletter is once a week, every Sunday.
+        Hey there, my name is Adam Navarro. I’m 29 years old and am a software
+        programmer, hobbyist musician and gym rat. Some of my favorite things in this
+        life are building things (usually with code) and helping others in whatever
+        way I am capable of. Below you can find some of my interests to get to know a
+        bit more about me. If you want to talk about something, please don’t hesitate
+        to reach out!
       </p>
     </Typography>
   );
