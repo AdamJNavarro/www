4c7f9582c460ae975dd408abee66ffe07a91b39f
Adam Navarro <adamjnav@gmail.com>
2022-08-03 11:27:11 -0500

    chore: tweak spotify grid spacing config

--- src/components/Music/Spotify/SpotifyGrid.tsx
+++ src/components/Music/Spotify/SpotifyGrid.tsx
@@ -9,10 +9,10 @@ export default function SpotifyGrid(props: DataGridProps) {
         spacing: 'xl',
         breakpoints: [
           { maxWidth: 'xl', cols: 2, spacing: 'xl' },
-          { maxWidth: 'lg', cols: 2, spacing: 'lg' },
-          { maxWidth: 'md', cols: 2, spacing: 'md' },
-          { maxWidth: 'sm', cols: 1, spacing: 'sm' },
-          { maxWidth: 'xs', cols: 1, spacing: 'xs' },
+          { maxWidth: 'lg', cols: 2, spacing: 'xl' },
+          { maxWidth: 'md', cols: 2, spacing: 'lg' },
+          { maxWidth: 'sm', cols: 1, spacing: 'lg' },
+          { maxWidth: 'xs', cols: 1, spacing: 'md' },
         ],
       }}
       placeholder={<SpotifyItem image={null} label="X" subLabel="x" url="/" />}
