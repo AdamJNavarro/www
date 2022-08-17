4d916b1fc4173f7c5af09bd99af06a2ffb16e64f
Adam Navarro <adamjnav@gmail.com>
2022-08-17 12:44:56 -0500

    chore: migrate to iframe and remove youtube deps

--- src/components/Dancing/DancingPerformances.tsx
+++ src/components/Dancing/DancingPerformances.tsx
@@ -1,7 +1,11 @@
-import { SimpleGrid } from '@mantine/core';
-import { YoutubeVideo } from '../common/Media';
+import { createStyles, SimpleGrid } from '@mantine/core';
 
-const youtubeVideos = [
+interface YoutubeEmbedProps {
+  videoId: string;
+  config: any;
+}
+
+const performanceVideos = [
   {
     videoId: 'yAk2TURgYew',
     config: {
@@ -18,11 +22,43 @@ const youtubeVideos = [
   },
 ];
 
+const useStyles = createStyles((theme) => ({
+  iframe: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    width: '100%',
+    height: '100%',
+    border: 0,
+    borderRadius: theme.radius.md,
+  },
+}));
+
+function buildYoutubeURL(videoId, config) {
+  return `https://www.youtube.com/embed/${videoId}?rel=0&modestbranding=1&start=${config.start}&end=${config.end}`;
+}
+
+function YoutubeEmbed({ videoId, config }: YoutubeEmbedProps) {
+  const { classes } = useStyles();
+  return (
+    <div style={{ display: 'flex', flexDirection: 'column' }}>
+      <div style={{ position: 'relative', width: '100%', paddingBottom: '56.25%' }}>
+        <iframe
+          className={classes.iframe}
+          src={buildYoutubeURL(videoId, config)}
+          title="Youtube Dance Performance Video"
+          allow="fullscreen"
+        />
+      </div>
+    </div>
+  );
+}
+
 export default function DancingPerformances() {
   return (
-    <SimpleGrid cols={1}>
-      {youtubeVideos.map((video) => (
-        <YoutubeVideo key={video.videoId} {...video} />
+    <SimpleGrid cols={1} spacing="xl">
+      {performanceVideos.map((video) => (
+        <YoutubeEmbed key={video.videoId} {...video} />
       ))}
     </SimpleGrid>
   );
