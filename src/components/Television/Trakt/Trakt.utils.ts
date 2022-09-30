1b1fd2b9f340205e43b4405b8c0e995068eb2ebc
Adam Navarro <adamjnav@gmail.com>
2022-09-30 14:25:27 -0500

    chore: remove trakt local storage fx and migrate back to env vars

--- src/components/Television/Trakt/Trakt.utils.ts
+++ src/components/Television/Trakt/Trakt.utils.ts
@@ -3,13 +3,6 @@ import useFetch, { IncomingOptions } from 'use-http';
 
 import { CustomFetchArgs, goFetch } from '~/utils';
 import {
-  areCredsExpired,
-  getLocalStorage,
-  LocalCreds,
-  localStorageKeys,
-  storeLocalCreds,
-} from '~/utils/Storage';
-import {
   TraktListEntry,
   TraktListFetchVars,
   TraktShow,
@@ -18,6 +11,7 @@ import {
 
 const TRAKT_CLIENT_ID = process.env.NEXT_PUBLIC_TRAKT_CLIENT_ID;
 const TRAKT_CLIENT_SECRET = process.env.NEXT_PUBLIC_TRAKT_CLIENT_SECRET;
+const TRAKT_ACCESS_TOKEN = process.env.NEXT_PUBLIC_TRAKT_ACCESS_TOKEN;
 const TRAKT_REFRESH_TOKEN = process.env.NEXT_PUBLIC_TRAKT_REFRESH_TOKEN;
 
 const TRAKT_USERNAME = 'adamjnavarro';
@@ -32,7 +26,7 @@ export const traktUrls = {
   stats: `${TRAKT_USER_BASE_URL}/stats`,
 };
 
-async function generateTraktCreds(refresh_token: string): Promise<LocalCreds> {
+async function generateTraktCreds(): Promise<any> {
   const res = await goFetch({
     url: TRAKT_TOKEN_URL,
     config: {
@@ -45,42 +39,21 @@ async function generateTraktCreds(refresh_token: string): Promise<LocalCreds> {
         client_secret: TRAKT_CLIENT_SECRET,
         redirect_uri: 'http://localhost:3000',
         grant_type: 'refresh_token',
-        refresh_token,
+        refresh_token: TRAKT_REFRESH_TOKEN,
       }),
     },
   });
 
   //if (res.error) return;
-  const creds = storeLocalCreds({
-    storageKey: localStorageKeys.traktCreds,
-    accessToken: res.data.access_token,
-    expiresIn: res.data.expires_in,
-    refreshToken: res.data.refresh_token,
-  });
-
-  return creds;
-}
-
-async function getTraktCreds(): Promise<LocalCreds> {
-  const creds = getLocalStorage(localStorageKeys.traktCreds);
-  if (!creds) {
-    return generateTraktCreds(TRAKT_REFRESH_TOKEN);
-  }
-
-  if (areCredsExpired(creds)) {
-    return generateTraktCreds(creds.refreshToken);
-  }
-  return creds;
+  return res;
 }
 
 const defaultFetchOpts: IncomingOptions = {
   interceptors: {
-    request: async ({ options }) => {
-      const creds = await getTraktCreds();
-
+    request: ({ options }) => {
       options.headers = {
         ...options.headers,
-        Authorization: `Bearer ${creds.accessToken}`,
+        Authorization: `Bearer ${TRAKT_ACCESS_TOKEN}`,
         'Content-Type': 'application/json',
         'trakt-api-key': TRAKT_CLIENT_ID,
         'trakt-api-version': '2',
