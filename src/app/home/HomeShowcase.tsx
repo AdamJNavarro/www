ddba88eb95a08e6f85aca6d9b4b85e36bffd821a
Adam Navarro <adamjnav@gmail.com>
2024-02-07 10:51:53 -0600

    chore: migrate home showcase photo to next image

--- src/app/home/HomeShowcase.tsx
+++ src/app/home/HomeShowcase.tsx
@@ -1,5 +1,6 @@
-import { Avatar, Box, Text } from '@mantine/core';
+import { Box, Text } from '@mantine/core';
 import { IconMoodHappy, IconSocial, IconStack2 } from '@tabler/icons-react';
+import Image from 'next/image';
 import NavGrid from '~/components/Navigation/NavGrid';
 import { NavLinkProps } from '~/components/Navigation/Navigation.types';
 import classes from './Showcase.module.css';
@@ -28,7 +29,16 @@ const items: NavLinkProps[] = [
 export default function HomeShowcase() {
   return (
     <Box className={classes.container}>
-      <Avatar src="/img/home-photo.jpg" radius={192 / 2} size={192} mx="auto" />
+      <Image
+        src="/img/home-photo.jpg"
+        priority
+        height={300}
+        width={300}
+        alt="adam-home-photo"
+        style={{
+          borderRadius: '50%',
+        }}
+      />
       <Text className={classes.title}>Adam Navarro</Text>
       <Text c="dimmed" className={classes.motto}>
         Find Passion. Foster Passion.
