767f16c96452aaa8f64a96c5fb922da0b09567c0
Adam Navarro <adamjnav@gmail.com>
2024-02-07 00:09:37 -0600

    chore: update letterboxd data

--- src/app/interests/movies/letterboxd/letterboxd.data.ts
+++ src/app/interests/movies/letterboxd/letterboxd.data.ts
@@ -1,33 +1,12 @@
-const lastUpdated = '02/06/2024';
+const lastUpdated = '02/07/2024';
 
-const watched = [
+ const watched = [
   {
-    posterPath: '/3ZnV9SohL2HT011c9MflLFaLfqv.jpg',
-    name: 'Gilly and Keeves: The Special',
-    date: '2023-12-21',
-    year: '2022',
-    url: 'https://boxd.it/D1oU',
-  },
-  {
-    posterPath: '/lsdismtCDga4vsKnmliz0h7CaZT.jpg',
-    name: 'The Iron Claw',
-    date: '2024-01-05',
+    posterPath: '/2e853FDVSIso600RqAMunPxiZjq.jpg',
+    name: 'Society of the Snow',
+    date: '2024-02-05',
     year: '2023',
-    url: 'https://boxd.it/w5Pm',
-  },
-  {
-    posterPath: '/xBFyJHvXCTgf9Vmq5uCnXRHrMQ4.jpg',
-    name: 'Ferrari',
-    date: '2024-01-17',
-    year: '2023',
-    url: 'https://boxd.it/cEtq',
-  },
-  {
-    posterPath: '/gSkfBGdxdialBMM7P02V4hcI6Ij.jpg',
-    name: 'Night Swim',
-    date: '2024-01-19',
-    year: '2024',
-    url: 'https://boxd.it/EyB0',
+    url: 'https://boxd.it/y9aw',
   },
   {
     posterPath: '/blPAS2HZcOGLTREbUeNIWmz0B6f.jpg',
@@ -37,42 +16,42 @@ const watched = [
     url: 'https://boxd.it/tJKq',
   },
   {
-    posterPath: '/2e853FDVSIso600RqAMunPxiZjq.jpg',
-    name: 'Society of the Snow',
-    date: '2024-02-05',
+    posterPath: '/gSkfBGdxdialBMM7P02V4hcI6Ij.jpg',
+    name: 'Night Swim',
+    date: '2024-01-19',
+    year: '2024',
+    url: 'https://boxd.it/EyB0',
+  },
+  {
+    posterPath: '/xBFyJHvXCTgf9Vmq5uCnXRHrMQ4.jpg',
+    name: 'Ferrari',
+    date: '2024-01-17',
     year: '2023',
-    url: 'https://boxd.it/y9aw',
+    url: 'https://boxd.it/cEtq',
+  },
+  {
+    posterPath: '/lsdismtCDga4vsKnmliz0h7CaZT.jpg',
+    name: 'The Iron Claw',
+    date: '2024-01-05',
+    year: '2023',
+    url: 'https://boxd.it/w5Pm',
+  },
+  {
+    posterPath: '/3ZnV9SohL2HT011c9MflLFaLfqv.jpg',
+    name: 'Gilly and Keeves: The Special',
+    date: '2023-12-21',
+    year: '2022',
+    url: 'https://boxd.it/D1oU',
   },
 ];
 
-const watchlist = [
+ const watchlist = [
   {
-    posterPath: '/39wmItIWsg5sZMyRUHLkWBcuVCM.jpg',
-    name: 'Spirited Away',
-    date: '2024-01-19',
-    year: '2001',
-    url: 'https://boxd.it/2b4m',
-  },
-  {
-    posterPath: '/kCGlIMHnOm8JPXq3rXM6c5wMxcT.jpg',
-    name: 'Poor Things',
-    date: '2024-01-19',
-    year: '2023',
-    url: 'https://boxd.it/tNWU',
-  },
-  {
-    posterPath: '/AbFtI353N2Pggl5TxfsI2VtpUt8.jpg',
-    name: 'The Zone of Interest',
-    date: '2024-01-29',
-    year: '2023',
-    url: 'https://boxd.it/gJsA',
-  },
-  {
-    posterPath: '/VHSzNBTwxV8vh7wylo7O9CLdac.jpg',
-    name: 'The Holdovers',
-    date: '2024-01-29',
-    year: '2023',
-    url: 'https://boxd.it/vHza',
+    posterPath: '/gAEUXC37vl1SnM7PXsHTF23I2vq.jpg',
+    name: 'Tarot',
+    date: '2024-02-02',
+    year: '2024',
+    url: 'https://boxd.it/qWMy',
   },
   {
     posterPath: '/pmr56P9w9jiRvMdoLTCmQ4iEURH.jpg',
@@ -82,12 +61,33 @@ const watchlist = [
     url: 'https://boxd.it/u5zu',
   },
   {
-    posterPath: '/gAEUXC37vl1SnM7PXsHTF23I2vq.jpg',
-    name: 'Tarot',
-    date: '2024-02-02',
-    year: '2024',
-    url: 'https://boxd.it/qWMy',
+    posterPath: '/VHSzNBTwxV8vh7wylo7O9CLdac.jpg',
+    name: 'The Holdovers',
+    date: '2024-01-29',
+    year: '2023',
+    url: 'https://boxd.it/vHza',
+  },
+  {
+    posterPath: '/AbFtI353N2Pggl5TxfsI2VtpUt8.jpg',
+    name: 'The Zone of Interest',
+    date: '2024-01-29',
+    year: '2023',
+    url: 'https://boxd.it/gJsA',
+  },
+  {
+    posterPath: '/kCGlIMHnOm8JPXq3rXM6c5wMxcT.jpg',
+    name: 'Poor Things',
+    date: '2024-01-19',
+    year: '2023',
+    url: 'https://boxd.it/tNWU',
+  },
+  {
+    posterPath: '/39wmItIWsg5sZMyRUHLkWBcuVCM.jpg',
+    name: 'Spirited Away',
+    date: '2024-01-19',
+    year: '2001',
+    url: 'https://boxd.it/2b4m',
   },
 ];
 
-export { lastUpdated, watched, watchlist };
+ export { lastUpdated, watched, watchlist };
