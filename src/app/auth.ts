ff63b8f9df826c4d21e1cf86dfb2a8c4adaabaad
Adam Navarro <adamjnav@gmail.com>
2024-02-16 14:11:45 -0600

    chore: use vercel env var to detect env instead of node env

--- src/app/auth.ts
+++ src/app/auth.ts
@@ -1,7 +1,7 @@
 import NextAuth from 'next-auth';
 import GitHub from 'next-auth/providers/github';
 
-const isDev = process.env.NODE_ENV === 'development';
+const isDev = (process.env.VERCEL_ENV as string) === 'development';
 
 export const {
   handlers: { GET, POST },
