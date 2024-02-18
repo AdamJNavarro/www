1ed71d8090a17c0044083f3eb69d3ea4ce2d5e51
Adam Navarro <adamjnav@gmail.com>
2024-02-17 20:03:07 -0600

    chore: refactor activity dashboard to render local data cards while external data cards are fetched

--- src/app/home/ActivityDashboard/index.tsx
+++ src/app/home/ActivityDashboard/index.tsx
@@ -1,56 +1,19 @@
-'use client';
-
 import { SimpleGrid } from '@mantine/core';
 import BookRead from './BookRead';
-import SongLiked from './SongLiked';
-import WordLearned from './WordLearned';
+
 import ShowWatched from './ShowWatched';
 import MovieWatched from './MovieWatched';
-import StarredRepo from './StarredRepo';
-import StravaSession from './StravaSession';
-import { WordProps } from '~/app/interests/words/Words.types';
-import Dashboard from './Dashboard';
 
-export type ActivityDashboardData = {
-  word: WordProps;
-  strava: any;
-  song: any;
-  repo: any;
-};
-
-export default function ActivityDashboard({
-  data,
-}: {
-  data: ActivityDashboardData;
-}) {
+export default function ActivityDashboard({ children }: { children: any }) {
   return (
     <SimpleGrid
       cols={{ base: 1, md: 2 }}
       spacing={{ base: 'md', sm: 'lg', lg: 'xl' }}
     >
-      <WordLearned data={data.word} />
-      <MovieWatched />
-      <StravaSession data={data.strava} />
-      <StarredRepo data={data.repo} />
       <BookRead />
-      <SongLiked data={data.song} />
+      <MovieWatched />
       <ShowWatched />
-    </SimpleGrid>
-  );
-}
-
-export function ActivityDashboardLoading() {
-  return (
-    <SimpleGrid
-      cols={{ base: 1, md: 2 }}
-      spacing={{ base: 'md', sm: 'lg', lg: 'xl' }}
-    >
-      <Dashboard.Loading />
-      <Dashboard.Loading />
-      <Dashboard.Loading />
-      <Dashboard.Loading />
-      <Dashboard.Loading />
-      <Dashboard.Loading />
+      {children}
     </SimpleGrid>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,15 @@ import { Suspense } from 'react';
 import { Page, Section } from '~/components/common';
 import { getLatestWord } from './data/db/queries';
 import HomeShowcase from './home/HomeShowcase';
-import ActivityDashboard, {
-  ActivityDashboardLoading,
-} from './home/ActivityDashboard';
+import ActivityDashboard from './home/ActivityDashboard';
 import { getLatestStarredRepo } from './data/github';
 import { getLatestStravaActivity } from './data/strava';
 import { getLatestLikedSongs } from './data/spotify';
+import WordLearned from './home/ActivityDashboard/WordLearned';
+import StravaSession from './home/ActivityDashboard/StravaSession';
+import StarredRepo from './home/ActivityDashboard/StarredRepo';
+import SongLiked from './home/ActivityDashboard/SongLiked';
+import Dashboard from './home/ActivityDashboard/Dashboard';
 
 export default async function HomePage() {
   return (
@@ -21,9 +24,20 @@ export default async function HomePage() {
             </Section.Title>
           </Section.Header>
           <Section.Content>
-            <Suspense fallback={<ActivityDashboardLoading />}>
-              <LatestActivities />
-            </Suspense>
+            <ActivityDashboard>
+              <Suspense fallback={<Dashboard.Loading />}>
+                <LatestWord />
+              </Suspense>
+              <Suspense fallback={<Dashboard.Loading />}>
+                <LatestStrava />
+              </Suspense>
+              <Suspense fallback={<Dashboard.Loading />}>
+                <LatestGithubRepoStarred />
+              </Suspense>
+              <Suspense fallback={<Dashboard.Loading />}>
+                <LatestSpotifyTrack />
+              </Suspense>
+            </ActivityDashboard>
           </Section.Content>
         </Section.Container>
       </Page.Content>
@@ -31,18 +45,25 @@ export default async function HomePage() {
   );
 }
 
-async function LatestActivities() {
-  const wordData = getLatestWord();
-  const repoData = getLatestStarredRepo();
-  const stravaData = getLatestStravaActivity();
-  const spotifyData = getLatestLikedSongs({ limit: 1 });
+async function LatestWord() {
+  const wordData = await getLatestWord();
 
-  const [word, repo, strava, song] = await Promise.all([
-    wordData,
-    repoData,
-    stravaData,
-    spotifyData,
-  ]);
+  return <WordLearned data={wordData} />;
+}
 
-  return <ActivityDashboard data={{ word, repo, strava, song }} />;
+async function LatestStrava() {
+  const stravaData = await getLatestStravaActivity();
+
+  return <StravaSession data={stravaData} />;
+}
+
+async function LatestGithubRepoStarred() {
+  const repoData = await getLatestStarredRepo();
+
+  return <StarredRepo data={repoData} />;
+}
+
+async function LatestSpotifyTrack() {
+  const data = await getLatestLikedSongs({ limit: 1 });
+  return <SongLiked data={data} />;
 }
