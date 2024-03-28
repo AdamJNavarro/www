3eb170515a612e88712d1c3157c5e91f0f73b317
Adam Navarro <adamjnav@gmail.com>
2024-03-28 15:23:28 -0500

    chore: update letterboxd data

--- src/app/movies/letterboxd/letterboxd.data.ts
+++ src/app/movies/letterboxd/letterboxd.data.ts
@@ -1,80 +1,92 @@
-const lastUpdated = '02/14/2024';
+const lastUpdated = '03/28/2024';
 
 const watched = [
   {
-    posterPath: '/ruyeAfmxbNPWZ92dWymqwTc6nWV.jpg',
-    name: 'The Zone of Interest',
-    year: '2023',
-    url: 'https://boxd.it/gJsA',
-  },
-  {
-    posterPath: '/2e853FDVSIso600RqAMunPxiZjq.jpg',
-    name: 'Society of the Snow',
-    year: '2023',
-    url: 'https://boxd.it/y9aw',
-  },
-  {
-    posterPath: '/blPAS2HZcOGLTREbUeNIWmz0B6f.jpg',
-    name: 'I.S.S.',
-    year: '2023',
-    url: 'https://boxd.it/tJKq',
-  },
-  {
-    posterPath: '/gf7byRrH3m0URyBQj78t21RfloF.jpg',
-    name: 'Night Swim',
+    posterPath: '/lF9LNf420WMH81gG7sq6ioDOhtl.jpg',
+    dateAdded: '2024-03-29',
+    name: 'Godzilla x Kong: The New Empire',
     year: '2024',
-    url: 'https://boxd.it/EyB0',
+    url: 'https://boxd.it/v1PK',
   },
   {
-    posterPath: '/xBFyJHvXCTgf9Vmq5uCnXRHrMQ4.jpg',
-    name: 'Ferrari',
-    year: '2023',
-    url: 'https://boxd.it/cEtq',
+    posterPath: '/zGVbrulkupqpbwgiNedkJPyQum4.jpg',
+    dateAdded: '2024-03-28',
+    name: 'My Hero Academia: Heroes Rising',
+    year: '2019',
+    url: 'https://boxd.it/lOQM',
   },
   {
-    posterPath: '/nfs7DCYhgrEIgxKYbITHTzKsggf.jpg',
-    name: 'The Iron Claw',
-    year: '2023',
-    url: 'https://boxd.it/w5Pm',
+    posterPath: '/hC4nTxdhXqFWzgqynGvvXVMiMNp.jpg',
+    dateAdded: '2024-03-28',
+    name: 'My Hero Academia: Two Heroes',
+    year: '2018',
+    url: 'https://boxd.it/igfw',
+  },
+  {
+    posterPath: '/bXi6IQiQDHD00JFio5ZSZOeRSBh.jpg',
+    dateAdded: '2024-03-26',
+    name: 'Road House',
+    year: '2024',
+    url: 'https://boxd.it/cdzQ',
+  },
+  {
+    posterPath: '/tBbr1LJn5rrP08Y16jJAKwLEECf.jpg',
+    dateAdded: '2024-03-18',
+    name: 'Hunter x Hunter: Phantom Rouge',
+    year: '2013',
+    url: 'https://boxd.it/6jPI',
+  },
+  {
+    posterPath: '/4eWeXswkAUIvdVWFvPrUFu2TxuI.jpg',
+    dateAdded: '2024-03-13',
+    name: 'Bob Marley: One Love',
+    year: '2024',
+    url: 'https://boxd.it/uckw',
   },
 ];
 
 const watchlist = [
   {
-    posterPath: '/rnUIdhByy6vZbWYm6I9m71Kmk3e.jpg',
+    posterPath: '/u3YQJctMzFN2wAvnkmXy41bXhFv.jpg',
+    dateAdded: '2024-03-20',
+    name: 'Late Night with the Devil',
+    year: '2023',
+    url: 'https://boxd.it/zo6G',
+  },
+  {
+    posterPath: '/l3CG9YMfRywp6FyrYpkZS4lThWM.jpg',
+    dateAdded: '2024-03-05',
+    name: 'Origin',
+    year: '2023',
+    url: 'https://boxd.it/shJs',
+  },
+  {
+    posterPath: '/s43rgDGRDOB12prgK9YfWPEIyAh.jpg',
+    dateAdded: '2024-02-17',
+    name: 'Sasquatch Sunset',
+    year: '2024',
+    url: 'https://boxd.it/Coz0',
+  },
+  {
+    posterPath: '/yvnIWt2j8VnDgwKJE2VMiFMa2Qo.jpg',
+    dateAdded: '2024-02-14',
     name: 'One Life',
     year: '2023',
     url: 'https://boxd.it/sAhC',
   },
   {
     posterPath: '/b4fn3VIdVTT3SX0rFMNFbl5xuvg.jpg',
+    dateAdded: '2024-02-14',
     name: 'Monkey Man',
     year: '2024',
     url: 'https://boxd.it/kvsO',
   },
   {
-    posterPath: '/bQ2ywkchIiaKLSEaMrcT6e29f91.jpg',
-    name: 'Godzilla x Kong: The New Empire',
-    year: '2024',
-    url: 'https://boxd.it/v1PK',
-  },
-  {
-    posterPath: '/uFtGe1CVW4Vk0ymN70xbtn81OcG.jpg',
+    posterPath: '/2DR8Wi785gwqCspLFVVr7G3sERx.jpg',
+    dateAdded: '2024-02-14',
     name: 'A Quiet Place: Day One',
     year: '2024',
     url: 'https://boxd.it/sDAi',
-  },
-  {
-    posterPath: '/fAjyUDKABSehK6pHWvh9ket5X3z.jpg',
-    name: 'Kingdom of the Planet of the Apes',
-    year: '2024',
-    url: 'https://boxd.it/ohEE',
-  },
-  {
-    posterPath: '/gAEUXC37vl1SnM7PXsHTF23I2vq.jpg',
-    name: 'Tarot',
-    year: '2024',
-    url: 'https://boxd.it/qWMy',
   },
 ];
 
