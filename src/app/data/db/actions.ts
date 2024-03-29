cb5c8472a21fa43d2140f9a2ba369fb1201dfa6c
Adam Navarro <AdamJNavarro@users.noreply.github.com>
2024-03-29 13:32:28 -0500

    chore: tweak format date util, change words card styling and impl wor… (#1538)

--- src/app/data/db/actions.ts
+++ src/app/data/db/actions.ts
@@ -4,8 +4,7 @@
 
 import { sql } from '@vercel/postgres';
 import { type Session } from 'next-auth';
-import { unstable_noStore as noStore } from 'next/cache';
-import { redirect } from 'next/navigation';
+import { unstable_noStore as noStore, revalidatePath } from 'next/cache';
 import { auth } from '~/app/auth';
 
 async function getSession(): Promise<Session> {
@@ -17,7 +16,7 @@ async function getSession(): Promise<Session> {
   return session;
 }
 
-export async function addWord(formData: any) {
+export async function addWord(formData: FormData) {
   if (!process.env.POSTGRES_URL) {
     return [];
   }
@@ -29,10 +28,13 @@ export async function addWord(formData: any) {
     throw new Error('Unauthorized');
   }
 
-  const { spelling, definition, partOfSpeech, dateLearned } = formData;
+  const spelling = formData.get('spelling')?.toString() || '';
+  const definition = formData.get('definition')?.toString() || '';
+  const partOfSpeech = formData.get('partOfSpeech')?.toString() || '';
+  const dateLearned = formData.get('dateLearned')?.toString() || '';
 
   noStore();
 
   await sql`INSERT INTO words (spelling, definition, part_of_speech, date_learned) VALUES (${spelling}, ${definition}, ${partOfSpeech}, ${dateLearned});`;
-  redirect('/words');
+  revalidatePath('/words');
 }
