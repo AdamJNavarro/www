e414fa8bc692f2fcee42aabc5ff69b2b48e4a81e
Adam Navarro <adamjnav@gmail.com>
2024-03-30 15:21:41 -0500

    chore: consolidate, refactor dancing performances and delete old file

--- src/app/dancing/page.tsx
+++ src/app/dancing/page.tsx
@@ -1,5 +1,4 @@
 import routes from '~/app/config/routes';
-import DancingPerformances from './DancingPerformances';
 import { Page } from '~/components/Layouts/Page';
 
 export const { metadata } = routes.dancing;
@@ -11,7 +10,31 @@ export default async function DancingPage() {
         <Page.Title>Dancing</Page.Title>
         <p>If there is a dance floor, you will find me on it.</p>
       </Page.Header>
-      <DancingPerformances />
+      <div className="mb-24">
+        <YoutubeEmbed videoId="yAk2TURgYew" config={{ start: 24, end: 76 }} />
+      </div>
+      <div className="mb-24">
+        <YoutubeEmbed videoId="oEpV40r7ho8" config={{ start: 210, end: 279 }} />
+      </div>
+    </div>
+  );
+}
+
+function buildYoutubeURL(videoId, config) {
+  return `https://www.youtube.com/embed/${videoId}?rel=0&modestbranding=1&start=${config.start}&end=${config.end}`;
+}
+
+function YoutubeEmbed({ videoId, config }: any) {
+  return (
+    <div className="flex flex-col">
+      <div className="relative w-full pb-[56.25%]">
+        <iframe
+          className="absolute top-0 left-0 w-full h-full border-0"
+          src={buildYoutubeURL(videoId, config)}
+          title="Youtube Dance Performance Video"
+          allow="fullscreen"
+        />
+      </div>
     </div>
   );
 }
