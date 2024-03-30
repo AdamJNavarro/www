c93dd01488c2f736528d4a8ad3fdbc5ac884c8de
Adam Navarro <AdamJNavarro@users.noreply.github.com>
2024-03-30 17:57:19 -0500

    chore: tweak tmdb poster to use h/w, suspense and server action (#1541)

--- src/app/data/tmdb/index.ts
+++ src/app/data/tmdb/index.ts
@@ -1,13 +1,22 @@
 'use server';
 
 import { MovieDb } from 'moviedb-promise';
+import { handleServerActionError } from '~/utils';
 
 const moviedb = new MovieDb(process.env.TMDB_KEY as string);
 
-export async function getTmdbPoster(id: number): Promise<string> {
-  const data = await moviedb.tvImages({ id, language: 'en' });
-  if (data.posters && data.posters.length) {
-    return `https://image.tmdb.org/t/p/original${data.posters[0].file_path}`;
+export async function getTmdbPoster(id: number): Promise<any> {
+  try {
+    const resp = await moviedb.tvImages({ id, language: 'en' });
+    if (resp.posters && resp.posters.length) {
+      return {
+        data: resp.posters[0],
+        error: null,
+      };
+    } else {
+      throw new Error();
+    }
+  } catch (error) {
+    handleServerActionError();
   }
-  return '';
 }
