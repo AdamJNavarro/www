f56a1bc810db142b298e7be6c5588853513eb78b
Adam Navarro <adamjnav@gmail.com>
2024-03-30 17:03:52 -0500

    chore: add en lang param to tmdb poster query

--- src/app/data/tmdb/index.ts
+++ src/app/data/tmdb/index.ts
@@ -5,7 +5,7 @@ import { MovieDb } from 'moviedb-promise';
 const moviedb = new MovieDb(process.env.TMDB_KEY as string);
 
 export async function getTmdbPoster(id: number): Promise<string> {
-  const data = await moviedb.tvImages({ id });
+  const data = await moviedb.tvImages({ id, language: 'en' });
   if (data.posters && data.posters.length) {
     return `https://image.tmdb.org/t/p/original${data.posters[0].file_path}`;
   }
