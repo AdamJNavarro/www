b7f31e84d7c471fe30046ada9828de3040ce2ba6
Adam Navarro <adamjnav@gmail.com>
2024-03-30 17:56:09 -0500

    chore: tweak tmdb poster to use h/w, suspense and server action

--- src/app/tv/trakt/TraktPoster.tsx
+++ src/app/tv/trakt/TraktPoster.tsx
@@ -1,8 +1,6 @@
-'use client';
-
 import Image from 'next/image';
+import { Suspense } from 'react';
 
-import { useEffect, useState } from 'react';
 import { getTmdbPoster } from '~/app/data/tmdb';
 
 interface TraktPosterProps {
@@ -10,26 +8,22 @@ interface TraktPosterProps {
   title: string;
 }
 
-export default function TraktPoster({ posterId, title }: TraktPosterProps) {
-  const [posterUrl, setPosterUrl] = useState<any>(null);
+export default async function TraktPoster({ posterId, title }: TraktPosterProps) {
+  const { data, error } = await getTmdbPoster(posterId);
 
-  useEffect(() => {
-    (async () => {
-      setPosterUrl(await getTmdbPoster(posterId));
-    })();
-  }, []);
-
-  if (!posterUrl) {
-    return <div className="animate-pulse w-16 h-24 dark:bg-slate-800/60" />;
-  }
+  if (error) return null;
 
   return (
-    <Image
-      src={posterUrl}
-      width={750}
-      height={1125}
-      alt={`${title}-tv-poster`}
-      className="w-16 h-24"
-    />
+    <Suspense
+      fallback={<div className="animate-pulse w-16 h-24 dark:bg-slate-800/60" />}
+    >
+      <Image
+        src={`https://image.tmdb.org/t/p/original${data.file_path}`}
+        width={data.width}
+        height={data.height}
+        alt={`${title}-tv-poster`}
+        className="w-16 h-24"
+      />
+    </Suspense>
   );
 }
