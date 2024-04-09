7c9121ffed2890c025c8d90ba4efb9df847c0fc9
Adam Navarro <adamjnav@gmail.com>
2024-04-09 15:20:52 -0500

    chore: add paragraphs to dancing page

--- src/app/dancing/page.tsx
+++ src/app/dancing/page.tsx
@@ -10,6 +10,49 @@ export default async function DancingPage() {
         <Page.Title>Dancing</Page.Title>
         <p>If there is a dance floor, you will find me on it.</p>
       </Page.Header>
+      <div className="prose mb-24">
+        <p>
+          It wasn’t until my college days that I realized just how much I enjoyed
+          dancing. At first I would need a dose of the liquid courage to be
+          comfortable enough to dance in public. Nowadays all that’s needed is some
+          good music and a space to move. I’ve found that most of my friends really
+          enjoy dancing but often need a little encouragement and guidance to let
+          loose. If you are able to, I would highly recommend being the person to
+          make dancing a more common and welcoming activity in your social circle. It
+          almost always leads to everyone having a better time and connecting with
+          others in a different way. Just be sure to respect when someone says they
+          don’t want to and keep it a judgement-free zone.
+        </p>
+        <p>
+          My favorite style of dance is without a doubt hip-hop. On occasion I will
+          pop into a salsa, bachata or merengue class but hip-hop is where my heart
+          truly is. Some of my favorite choreographers are{' '}
+          <a
+            href="https://www.juliandeguzman.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Julian Deguzman
+          </a>
+          ,{' '}
+          <a
+            href="https://www.instagram.com/jasper_sanchez"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Jasper Sanchez
+          </a>{' '}
+          and{' '}
+          <a
+            href="https://www.instagram.com/dvtboinvte"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Nate Ramos
+          </a>
+          .
+        </p>
+      </div>
       <div className="mb-24">
         <YoutubeEmbed videoId="yAk2TURgYew" config={{ start: 24, end: 76 }} />
       </div>
