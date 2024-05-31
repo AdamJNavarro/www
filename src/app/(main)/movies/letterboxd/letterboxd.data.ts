36c96012ca30026e956f205327e0c27066f9aa09
Adam Navarro <adamjnav@gmail.com>
2024-05-31 09:31:25 -0500

    chore: upd letterboxd data

--- src/app/(main)/movies/letterboxd/letterboxd.data.ts
+++ src/app/(main)/movies/letterboxd/letterboxd.data.ts
@@ -1,6 +1,13 @@
-const lastUpdated = '2024-05-21T18:50:52.338Z';
+const lastUpdated = '2024-05-31T14:30:54Z';
 
 const watched = [
+  {
+    posterPath: '/gKkl37BQuKTanygYQG1pyYgLVgf.jpg',
+    dateAdded: '2024-05-30',
+    name: 'Kingdom of the Planet of the Apes',
+    year: '2024',
+    url: 'https://boxd.it/ohEE',
+  },
   {
     posterPath: '/gAEUXC37vl1SnM7PXsHTF23I2vq.jpg',
     dateAdded: '2024-05-22',
@@ -36,57 +43,50 @@ const watched = [
     year: '2024',
     url: 'https://boxd.it/z1jW',
   },
-  {
-    posterPath: '/jzP5rPwwNqUGERlrUzKKljfyulf.jpg',
-    dateAdded: '2024-03-30',
-    name: "My Hero Academia: World Heroes' Mission",
-    year: '2021',
-    url: 'https://boxd.it/sTw2',
-  },
 ];
 
 const watchlist = [
+  {
+    posterPath: '/4rXqTMlkEaMiJjiG0Z2BX6F6Dkm.jpg',
+    dateAdded: '2024-05-26',
+    name: 'Avatar 3',
+    year: '2025',
+    url: 'https://boxd.it/2W7s',
+  },
+  {
+    posterPath: '/mRhEg5hBCSi2aOdercqLC3AmT0A.jpg',
+    dateAdded: '2024-05-26',
+    name: 'Mission: Impossible 8',
+    year: '2025',
+    url: 'https://boxd.it/l7A2',
+  },
+  {
+    posterPath: '/qwDWC2OzPw9zGc2lVCbjDrdaL6A.jpg',
+    dateAdded: '2024-05-26',
+    name: 'The Batman - Part II',
+    year: '2026',
+    url: 'https://boxd.it/umDy',
+  },
+  {
+    posterPath: '/jbwYaoYWZwxtPP76AZnfYKQjCEB.jpg',
+    dateAdded: '2024-05-26',
+    name: 'Deadpool & Wolverine',
+    year: '2024',
+    url: 'https://boxd.it/jq8s',
+  },
+  {
+    posterPath: '/iADOJ8Zymht2JPMoy3R7xceZprc.jpg',
+    dateAdded: '2024-05-26',
+    name: 'Furiosa: A Mad Max Saga',
+    year: '2024',
+    url: 'https://boxd.it/tABk',
+  },
   {
     posterPath: '/sWZRLME3WZwVKvAfUFh0sepQiDh.jpg',
     dateAdded: '2024-05-21',
     name: 'Never Let Go',
     year: '2024',
     url: 'https://boxd.it/uGn6',
-  },
-  {
-    posterPath: '/5aj8vVGFwGVbQQs26ywhg4Zxk2L.jpg',
-    dateAdded: '2024-05-21',
-    name: 'Longlegs',
-    year: '2024',
-    url: 'https://boxd.it/KzLM',
-  },
-  {
-    posterPath: '/k7bclFV4hWIdgNcLileBKGWFIa9.jpg',
-    dateAdded: '2024-05-21',
-    name: 'Alien: Romulus',
-    year: '2024',
-    url: 'https://boxd.it/zGqO',
-  },
-  {
-    posterPath: '/u3YQJctMzFN2wAvnkmXy41bXhFv.jpg',
-    dateAdded: '2024-03-20',
-    name: 'Late Night with the Devil',
-    year: '2023',
-    url: 'https://boxd.it/zo6G',
-  },
-  {
-    posterPath: '/l3CG9YMfRywp6FyrYpkZS4lThWM.jpg',
-    dateAdded: '2024-03-05',
-    name: 'Origin',
-    year: '2023',
-    url: 'https://boxd.it/shJs',
-  },
-  {
-    posterPath: '/uZNdH2zfws2BIy9ENtoFTFp0hPG.jpg',
-    dateAdded: '2024-02-17',
-    name: 'Sasquatch Sunset',
-    year: '2024',
-    url: 'https://boxd.it/Coz0',
   },
 ];
 
