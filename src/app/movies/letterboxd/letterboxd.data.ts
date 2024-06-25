a224f8c199740888bda59d16de9e08637aea552a
Adam Navarro <adamjnav@gmail.com>
2024-06-25 14:07:39 -0500

    chore: upd letterboxd data

--- src/app/movies/letterboxd/letterboxd.data.ts
+++ src/app/movies/letterboxd/letterboxd.data.ts
@@ -1,93 +1,93 @@
-const lastUpdated = '2024-05-31T14:30:54Z';
+const lastUpdated = '2024-06-25T19:07:09Z' 
 
-const watched = [
+ const watched = [
   {
-    posterPath: '/gKkl37BQuKTanygYQG1pyYgLVgf.jpg',
-    dateAdded: '2024-05-30',
-    name: 'Kingdom of the Planet of the Apes',
-    year: '2024',
-    url: 'https://boxd.it/ohEE',
+    "posterPath": "/9ocVEG3GMOrMGLWINf6RHS4rnJM.jpg",
+    "dateAdded": "2024-06-26",
+    "name": "The Bikeriders",
+    "year": "2023",
+    "url": "https://boxd.it/C6Ym"
   },
   {
-    posterPath: '/gAEUXC37vl1SnM7PXsHTF23I2vq.jpg',
-    dateAdded: '2024-05-22',
-    name: 'Tarot',
-    year: '2024',
-    url: 'https://boxd.it/qWMy',
+    "posterPath": "/iADOJ8Zymht2JPMoy3R7xceZprc.jpg",
+    "dateAdded": "2024-06-05",
+    "name": "Furiosa: A Mad Max Saga",
+    "year": "2024",
+    "url": "https://boxd.it/tABk"
   },
   {
-    posterPath: '/qyT2xw9FBxHlNXQYsuNCu8T7Rbo.jpg',
-    dateAdded: '2024-05-21',
-    name: 'The Strangers: Chapter 1',
-    year: '2024',
-    url: 'https://boxd.it/CcEM',
+    "posterPath": "/gKkl37BQuKTanygYQG1pyYgLVgf.jpg",
+    "dateAdded": "2024-05-30",
+    "name": "Kingdom of the Planet of the Apes",
+    "year": "2024",
+    "url": "https://boxd.it/ohEE"
   },
   {
-    posterPath: '/9YEGawvjaRgnyW6QVcUhFJPFDco.jpg',
-    dateAdded: '2024-05-21',
-    name: 'Black Clover: Sword of the Wizard King',
-    year: '2023',
-    url: 'https://boxd.it/uAqo',
+    "posterPath": "/gAEUXC37vl1SnM7PXsHTF23I2vq.jpg",
+    "dateAdded": "2024-05-22",
+    "name": "Tarot",
+    "year": "2024",
+    "url": "https://boxd.it/qWMy"
   },
   {
-    posterPath: '/8aF0iAKH9MJMYAZdi0Slg77RYa2.jpg',
-    dateAdded: '2024-05-01',
-    name: 'The Ministry of Ungentlemanly Warfare',
-    year: '2024',
-    url: 'https://boxd.it/u5zu',
+    "posterPath": "/cdFZoQDxLRRWXLB3kxCnc674dBF.jpg",
+    "dateAdded": "2024-05-21",
+    "name": "The Strangers: Chapter 1",
+    "year": "2024",
+    "url": "https://boxd.it/CcEM"
   },
   {
-    posterPath: '/sh7Rg8Er3tFcN9BpKIPOMvALgZd.jpg',
-    dateAdded: '2024-04-24',
-    name: 'Civil War',
-    year: '2024',
-    url: 'https://boxd.it/z1jW',
-  },
-];
+    "posterPath": "/9YEGawvjaRgnyW6QVcUhFJPFDco.jpg",
+    "dateAdded": "2024-05-21",
+    "name": "Black Clover: Sword of the Wizard King",
+    "year": "2023",
+    "url": "https://boxd.it/uAqo"
+  }
+] 
 
-const watchlist = [
+ const watchlist = [
   {
-    posterPath: '/4rXqTMlkEaMiJjiG0Z2BX6F6Dkm.jpg',
-    dateAdded: '2024-05-26',
-    name: 'Avatar 3',
-    year: '2025',
-    url: 'https://boxd.it/2W7s',
+    "posterPath": "/6zyl8uGWYjcCb8CJVMk7BZK0SfU.jpg",
+    "dateAdded": "2024-06-06",
+    "name": "The Guest",
+    "year": "2014",
+    "url": "https://boxd.it/76RS"
   },
   {
-    posterPath: '/mRhEg5hBCSi2aOdercqLC3AmT0A.jpg',
-    dateAdded: '2024-05-26',
-    name: 'Mission: Impossible 8',
-    year: '2025',
-    url: 'https://boxd.it/l7A2',
+    "posterPath": "/eZXoA5BjUxm1fdAKb3lPvbDY2L8.jpg",
+    "dateAdded": "2024-05-26",
+    "name": "Avatar 3",
+    "year": "2025",
+    "url": "https://boxd.it/2W7s"
   },
   {
-    posterPath: '/qwDWC2OzPw9zGc2lVCbjDrdaL6A.jpg',
-    dateAdded: '2024-05-26',
-    name: 'The Batman - Part II',
-    year: '2026',
-    url: 'https://boxd.it/umDy',
+    "posterPath": "/mRhEg5hBCSi2aOdercqLC3AmT0A.jpg",
+    "dateAdded": "2024-05-26",
+    "name": "Mission: Impossible 8",
+    "year": "2025",
+    "url": "https://boxd.it/l7A2"
   },
   {
-    posterPath: '/jbwYaoYWZwxtPP76AZnfYKQjCEB.jpg',
-    dateAdded: '2024-05-26',
-    name: 'Deadpool & Wolverine',
-    year: '2024',
-    url: 'https://boxd.it/jq8s',
+    "posterPath": "/hUe1G6Ziwl8b6DaaGHjhG6LQQH8.jpg",
+    "dateAdded": "2024-05-26",
+    "name": "The Batman - Part II",
+    "year": "2026",
+    "url": "https://boxd.it/umDy"
   },
   {
-    posterPath: '/iADOJ8Zymht2JPMoy3R7xceZprc.jpg',
-    dateAdded: '2024-05-26',
-    name: 'Furiosa: A Mad Max Saga',
-    year: '2024',
-    url: 'https://boxd.it/tABk',
+    "posterPath": "/jbwYaoYWZwxtPP76AZnfYKQjCEB.jpg",
+    "dateAdded": "2024-05-26",
+    "name": "Deadpool & Wolverine",
+    "year": "2024",
+    "url": "https://boxd.it/jq8s"
   },
   {
-    posterPath: '/sWZRLME3WZwVKvAfUFh0sepQiDh.jpg',
-    dateAdded: '2024-05-21',
-    name: 'Never Let Go',
-    year: '2024',
-    url: 'https://boxd.it/uGn6',
-  },
-];
+    "posterPath": "/sWZRLME3WZwVKvAfUFh0sepQiDh.jpg",
+    "dateAdded": "2024-05-21",
+    "name": "Never Let Go",
+    "year": "2024",
+    "url": "https://boxd.it/uGn6"
+  }
+] 
 
-export { lastUpdated, watched, watchlist };
+ export { lastUpdated, watched, watchlist };
