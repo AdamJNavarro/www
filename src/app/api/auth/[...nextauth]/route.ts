2052bf7056df8cee7b83983a153c0f6cf2dcec11
Adam Navarro <adamjnav@gmail.com>
2025-01-24 12:11:34 -0600

    chore: remove next codemod comments

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,4 +1,2 @@
-export { /* @next-codemod-error `GET` export is re-exported. Check if this component uses `params` or `searchParams`*/
-GET, /* @next-codemod-error `POST` export is re-exported. Check if this component uses `params` or `searchParams`*/
-POST } from '~/app/auth';
+export { GET, POST } from '~/app/auth';
 export const runtime = 'edge';
